13c4351f4608a1b7c01d853d3e41a14e4f1351b4
tranceyos2419 <31304661+tranceyos2419@users.noreply.github.com>
2020-03-05 11:24:23 +0900

    finish setting up counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
       <h2>App</h2>
+      <Counter />
     </div>
   );
 }
---
+++ src/components/Counter.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {}
+
+const Counter: React.FC<Props> = () => {
+  return (
+    <div>
+      <h2>Counter</h2>
+    </div>
+  );
+};
+
+export default Counter;
